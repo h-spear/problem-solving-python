84f88396b7bdd54ecd362465119708aec62c60e6
hspear <gusckd96@naver.com>
2021-08-18 23:39:32 +0900

    dfs,bfs

--- samsung/youth_shark.py
+++ samsung/youth_shark.py
@@ -1,54 +1,85 @@
 # https://www.acmicpc.net/problem/19236
+import copy
+from os import path
 
-from collections import deque
-import numpy
-
-graph = []
-fishes = dict()
-shark_x, shark_y,shark_dir = 0,0,0
-
+graph = [[None] * 4 for _ in range(4)]
 
 for i in range(4):
-  input_data = list(map(int, input().split()))
-  row = []
-  for j in range(0,8,2):
-    row.append((input_data[j],input_data[j+1]))
-    fishes[input_data[j]] = (i,j//2)
-  graph.append(row)
+    input_data = list(map(int, input().split()))
+    for j in range(0, 8, 2):
+        graph[i][j // 2] = [input_data[j], input_data[j + 1] - 1]
 
 
-# 99 : shark
-def eating(x,y):
-  global shark_x, shark_y, shark_dir
-  global fishes
-  shark_x, shark_y = x, y
-  shark_dir = graph[x][y][1]
-  fishes[graph[x][y][0]] = (-1,-1)
-  graph[x][y] = (99, shark_dir)
-
-# 1,2,3,4,5,6,7,8
 # Up, UL, Left, LD, Down, DR, Right, RU
 dx = [-1, -1, 0, 1, 1, 1, 0, -1]
 dy = [0, -1, -1, -1, 0, 1, 1, 1]
 
-def fishMoving(graph, fishes):
-  for j in range(1,len(fishes)):
-    x, y = fishes[j]
-    if x == y == -1:
-      continue
-    dir = graph[x][y][1]
-    for i in range(8):
-      nx = x + dx[(dir + i - 1) % 8]
-      ny = y + dy[(dir + i - 1) % 8]
-      if nx >= 0 and nx < 4 and ny >= 0 and ny < 4 and graph[nx][ny][0] != 99:
-        graph[nx][ny], graph[x][y] = graph[x][y], graph[nx][ny]
-        fishes[j], fishes[graph[x][y][0]] = fishes[graph[x][y][0]], fishes[j]
-        break
+
+def turnLeft(direction):
+    return (direction + 1) % 8
 
 
-def dfs(x,y):
-  if 
+def findFish(graph, num):
+    for i in range(4):
+        for j in range(4):
+            if graph[i][j][0] == num:
+                return i, j
+    return None
+
+def fishMove(graph,now_x,now_y):
+    for j in range(1, 17):
+        fish = findFish(graph, j)
+        if fish == None:
+            continue
+        x, y = fish
+        dir = graph[x][y][1]
+        for _ in range(8):
+            nx = x + dx[dir]
+            ny = y + dy[dir]
+
+            if (
+                nx >= 0
+                and nx < 4
+                and ny >= 0
+                and ny < 4
+                and graph[nx][ny][0] != -1
+            ):
+              if not (nx == now_x and ny == now_y):
+                graph[nx][ny], graph[x][y] = graph[x][y], graph[nx][ny]
+                break
+            dir = turnLeft(dir)
+    return graph
 
 
-while True:
-  eating(shark_x,shark_y)
+def possiblePath(shark_x, shark_y, shark_dir):
+    path = []
+    while True:
+        shark_x = shark_x + dx[shark_dir]
+        shark_y = shark_y + dy[shark_dir]
+        if shark_x < 0 or shark_x >= 4 or shark_y < 0 or shark_y >= 4:
+            break
+        path.append((shark_x, shark_y))
+    return path
+
+
+result = 0
+
+def dfs(graph, x, y, sum):
+    global result
+    graph = copy.deepcopy(graph)
+    sum += graph[x][y][0]
+    graph[x][y][0] = -1
+    direction = graph[x][y][1]
+    fishMove(graph,x,y)
+
+    path = possiblePath(x,y,direction)
+    if len(path) == 0:
+      result = max(result, sum)
+      return 
+    for next_x, next_y in path:
+      dfs(graph,next_x,next_y,sum)
+
+
+
+dfs(graph, 0,0,0)
+print(result)
