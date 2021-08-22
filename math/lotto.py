c6b013b2de4e3e79843b02301d8fa3d80b8ffff1
hspear <gusckd96@naver.com>
2021-08-22 17:07:21 +0900

    solve problems 5:7

---
+++ math/lotto.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/6603
+
+from itertools import combinations
+
+data = []
+while True:
+  input_data = list(map(int, input().split()))
+  if input_data == [0]:
+    break
+  data.append(input_data)
+  
+for lotto in data:
+  for array in combinations(lotto[1:],6):
+    for x in array:
+      print(x, end =' ')
+    print()
+  print()
