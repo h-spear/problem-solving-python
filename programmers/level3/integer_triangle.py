e1fd0623fa8a7157f8f164a1036bfec0b746f421
h-spear <80024307+h-spear@users.noreply.github.com>
2022-03-04 11:26:37 +0900

    easy programmers problems

---
+++ programmers/level3/integer_triangle.py
@@ -0,0 +1,13 @@
+# https://programmers.co.kr/learn/courses/30/lessons/43105
+
+
+def solution(triangle):
+    n = len(triangle)
+    dp = [[0] * n for _ in range(n)]
+    dp[0][0] = triangle[0][0]
+    for i in range(n - 1):
+        for j in range(i + 1):
+            dp[i + 1][j] = max(dp[i + 1][j], triangle[i + 1][j] + dp[i][j])
+            dp[i + 1][j + 1] = max(dp[i + 1][j + 1], triangle[i + 1][j + 1] + dp[i][j])
+
+    return max(dp[n - 1])
---
+++ programmers/level3/way_to_school.py
@@ -0,0 +1,22 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42898
+
+
+def solution(m, n, puddles):
+    p = 1000000007
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        if [1, i] in puddles:
+            break
+        dp[i][1] = 1
+    for j in range(1, m + 1):
+        if [j, 1] in puddles:
+            break
+        dp[1][j] = 1
+
+    for i in range(2, n + 1):
+        for j in range(2, m + 1):
+            if [j, i] in puddles:
+                continue
+            dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % p
+
+    return dp[n][m]
