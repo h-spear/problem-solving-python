984785db29f0f635cd1fd1839cd6adfc232c7719
h-spear <80024307+h-spear@users.noreply.github.com>
2022-04-29 00:25:02 +0900

    RC4 algorithm

--- security/RC4_number.py
+++ security/RC4_number.py
@@ -1,9 +1,9 @@
+import random
+
 bits = 256
 
 
 def key_schedling(key):
-    key = key.encode()
-
     # initialize
     key_length = len(key)
     S = [i for i in range(bits)]
@@ -23,8 +23,7 @@ def encrypt(message, key):
     j = 0
     S = key_schedling(key)
     stream_key = []
-    message = message.encode()
-    cipher_text = bytearray()
+    cipher_text = []
 
     for k in range(len(message)):
         i = (i + 1) % bits
@@ -40,7 +39,7 @@ def decrypt(message, key):
     j = 0
     S = key_schedling(key)
     stream_key = []
-    plain_text = bytearray()
+    plain_text = []
 
     for k in range(len(message)):
         i = (i + 1) % bits
@@ -51,13 +50,24 @@ def decrypt(message, key):
     return plain_text
 
 
-# message = input("plaintext: ")
-# key = input("key: ")
-message = "HELLO WORLD! THIS IS STUDY"
-key = "COMPUTER SCIENCE"
+message = []
+key = []
 
+# random으로 10글자 메시지 생성
+for i in range(10):
+    message.append(random.randrange(0, bits))
+
+# key는 random으로 4글자 생성
+for i in range(4):
+    key.append(random.randrange(0, bits))
+
+print("message:", message)
+print("key:", key)
+
+# 암호화
 cipher_text = encrypt(message, key)
 print("ciphertext:", cipher_text)
 
+# 복호화
 original_text = decrypt(cipher_text, key)
 print("decrypted:", original_text)
