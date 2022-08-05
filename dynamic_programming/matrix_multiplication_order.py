81e094e82de9010b370dd11cf759764a15900df9
h-spear <80024307+h-spear@users.noreply.github.com>
2022-08-06 02:44:47 +0900

    study matrix dp problem

---
+++ dynamic_programming/matrix_multiplication_order.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/11049
+# https://www.youtube.com/watch?v=u1gIkGXUqBQ
+
+n = int(input())
+matrix = []
+for _ in range(n):
+    matrix.append(tuple(map(int, input().split())))
+
+dp = [[0] * n for _ in range(n)]
+
+cnt = 0
+for i in range(1, n):
+    for j in range(n - i):
+        x = j + i
+        dp[j][x] = 2 ** 32
+        for k in range(j, x):
+            dp[j][x] = min(
+                dp[j][x],
+                dp[j][k] + dp[k + 1][x] + matrix[j][0] * matrix[k][1] * matrix[x][1],
+            )
+
+print(matrix[0][n - 1])
