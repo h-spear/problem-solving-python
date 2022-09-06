f43b529ee0e663100a6761758738ae1c0bc90060
h-spear <80024307+h-spear@users.noreply.github.com>
2022-09-07 03:02:11 +0900

    update note

---
+++ my_note/minimum_vertex_cover.py
@@ -0,0 +1,16 @@
+# Vertex Cover
+# - 그래프 간선의 양 끝점 중 하나 이상이 Vertex Cover에 포함
+# - 전체 그래프도 Vertex Cover
+#
+# Minimum Vertex Cover
+# - Vertex Cover 중에서 크기가 가장 작은 Vertex Cover
+# - 그래프 간선의 양 끝점 중 적어도 하나는 Vertex Cover에 포함
+
+# Minimum Vertex Cover와 Bipartite Matching
+# Konig's theorem(쾨닉의 정리)
+# ** bipartite graph ** 에서 Maximum Matching == Minimum Vertex Cover size
+
+
+# https://www.acmicpc.net/problem/1867
+# https://www.acmicpc.net/problem/11014
+# https://www.acmicpc.net/problem/2414
