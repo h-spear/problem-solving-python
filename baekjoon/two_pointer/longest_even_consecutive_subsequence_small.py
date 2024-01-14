142cc37746253fc41dce1099322b41990db8a994
h-spear <80024307+h-spear@users.noreply.github.com>
2024-01-14 21:20:24 +0900

    solve two pointer problem

---
+++ baekjoon/two_pointer/longest_even_consecutive_subsequence_small.py
@@ -0,0 +1,28 @@
+# https://www.acmicpc.net/problem/22857
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def main():
+    N, K = map(int, input().split())
+    S = list(map(int, input().split()))
+    left = 0
+    odd_cnt = 0
+    answer = 0
+    for right in range(0, N):
+        if S[right] & 1:
+            odd_cnt += 1
+
+        while odd_cnt > K:
+            odd_cnt -= S[left] & 1
+            left += 1
+
+        answer = max(answer, right - left - odd_cnt + 1)
+
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
